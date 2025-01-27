5a4432e7b728a67a529165be7c36e8a4d49e7b8f
ivtherapy123 <yura00089@mail.ru>
2025-01-27 16:13:43 +0300

    Add files via upload

--- api pen xc.go
+++ api pen xc.go
@@ -2,26 +2,29 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
 	"github.com/gorilla/mux"
 	"net/http"
 )
 
-var task requestBody
-
-type requestBody struct {
-	Message string `json:"message"`
-}
+var task Message
 
 func HelloHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Hello,", task)
+	var tasks []Message
+	DB.Find(&tasks)
+	json.NewEncoder(w).Encode(tasks)
 }
 
 func PostHandler(w http.ResponseWriter, r *http.Request) {
 	json.NewDecoder(r.Body).Decode(&task)
+	DB.Create(&task)
+	json.NewEncoder(w).Encode(task)``
 }
 
 func main() {
+	InitDB()
+
+	DB.AutoMigrate(&Message{})
+
 	router := mux.NewRouter()
 	router.HandleFunc("/api/hello", HelloHandler).Methods("GET")
 	router.HandleFunc("/api/post", PostHandler).Methods("POST")
